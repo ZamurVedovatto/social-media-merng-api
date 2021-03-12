76f486971c5fd36f09bc5686025ec8faa4b2658c
Zamur Vedovatto <vedovatto@gmail.com>
2021-03-12 12:19:14 -0300

    connect to mongo/set models typedefs and resolvers

---
+++ models/Post.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+
+const postSchema = new mongoose.Schema({
+  body: String,
+  username: String,
+  createdAt: String,
+  comments: [
+    {
+      body: String,
+      username: String,
+      createdAt: String,
+    }
+  ],
+  likes: [
+    {
+      username: String,
+      createdAt: String,
+    }
+  ],
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'users'
+  }
+})
+
+const post = mongoose.model('Post', postSchema)
+module.exports = post;
